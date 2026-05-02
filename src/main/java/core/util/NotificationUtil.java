2ef2aace489ad66d04419e47f2236aa59ed48924
c836202 <nelio.muchisse@kwalityprojects.co.mz>
2026-05-02 23:54:12 +0200

    send email service

--- src/main/java/core/util/NotificationUtil.java
+++ src/main/java/core/util/NotificationUtil.java
@@ -1,10 +1,10 @@
 package core.util;
 
-import core.beans.CustomerResponse;
 import core.beans.SMSContext;
 import core.constants.NotificationTypes;
 import core.threads.PostSMSCreate;
 import dao.entities.InsurancePolicy;
+import dao.entities.PolicyHolder;
 import dao.interfaces.InsurancePolicyInterface;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -28,29 +28,33 @@ public class NotificationUtil {
     @Inject
     private InsurancePolicyInterface insurancePolicyInterface;
 
-    public void postSendSMS(String traceId, InsurancePolicy savedInsurancePolicy) {
-        /*try {
+    public void postSendSMS(String traceId, InsurancePolicy insurancePolicy) {
+        try {
 
-            String body = queryUtil.getNotificationBody(NotificationTypes.POLICY_ISSUE.toString()).replace("[First Name]", customerResponse.getCoreCustomer().getCustomerName()
-                    ).replace("[Insurance Type]", savedInsurancePolicy.getSubProduct().getDescription())
-                    .replace("[Policy Number]", savedInsurancePolicy.getPolicyId());
-            setSMSContext(customerResponse, body, traceId, NotificationTypes.POLICY_ISSUE.toString(), savedInsurancePolicy.getPolicyId());
+            PolicyHolder policyHolder = insurancePolicy.getPolicyHolder();
+
+            String body = queryUtil.getNotificationBody(NotificationTypes.POLICY_ISSUE.toString()).replace("[First Name]",
+                            policyHolder.getCustomerName()
+                    ).replace("[Insurance Type]", insurancePolicy.getSubProduct().getDescription())
+                    .replace("[Policy Number]", insurancePolicy.getPolicyId());
+
+            setSMSContext(policyHolder.getMobileNumber(), body, traceId, NotificationTypes.POLICY_ISSUE.toString(), insurancePolicy.getPolicyId());
         }
         catch (Exception e){
 
             LOGGER.error(e);
-        } */
+        }
     }
 
-    public void postSendSMS(String traceId, String insurancePolicyId, CustomerResponse customerResponse) {
+    public void postSendSMS(String traceId, String insurancePolicyId, String mobileNumber, String customerName) {
         try {
 
             Optional<InsurancePolicy> insurancePolicy = insurancePolicyInterface.findByPolicyId(insurancePolicyId);
             if(insurancePolicy.isPresent()) {
-                String body = queryUtil.getNotificationBody(NotificationTypes.POLICY_ISSUE.toString()).replace("[First Name]", customerResponse.getCoreCustomer().getCustomerName()
+                String body = queryUtil.getNotificationBody(NotificationTypes.POLICY_ISSUE.toString()).replace("[First Name]", customerName
                         ).replace("[Insurance Type]", insurancePolicy.get().getSubProduct().getDescription())
                         .replace("[Policy Number]", insurancePolicyId);
-                setSMSContext(customerResponse, body, traceId, NotificationTypes.POLICY_ISSUE.toString(), insurancePolicyId);
+                setSMSContext(mobileNumber, body, traceId, NotificationTypes.POLICY_ISSUE.toString(), insurancePolicyId);
             }
             else {
                 LOGGER.warn("Policy not found for id -> {}",insurancePolicyId);
@@ -61,18 +65,18 @@ public class NotificationUtil {
         }
     }
 
-    public void postSendAmendmentSMS(String traceId, InsurancePolicy savedInsurancePolicy, CustomerResponse customerResponse) {
+    public void postSendAmendmentSMS(String traceId, InsurancePolicy savedInsurancePolicy, String mobileNumber) {
         String body = queryUtil.getNotificationBody(NotificationTypes.AMENDMENT.toString()).
                 replace("[Insurance Type]", savedInsurancePolicy.getSubProduct().getDescription())
                 .replace("[Police Number]", savedInsurancePolicy.getPolicyId());
-             setSMSContext(customerResponse, body, traceId, NotificationTypes.POLICY_ISSUE.toString(), savedInsurancePolicy.getPolicyId());
+             setSMSContext(mobileNumber, body, traceId, NotificationTypes.POLICY_ISSUE.toString(), savedInsurancePolicy.getPolicyId());
     }
 
-    public void setSMSContext(CustomerResponse customerResponse, String body, String logId, String notificationType, String transactionId) {
+    public void setSMSContext(String mobileNumber, String body, String logId, String notificationType, String transactionId) {
         try {
             SMSContext smsContext = new SMSContext();
             smsContext.setBody(body);
-            smsContext.setMobileNumber(customerResponse.getCoreCustomer().getMobileNumber().trim());
+            smsContext.setMobileNumber(mobileNumber);
             smsContext.setLogId(logId);
             smsContext.setNotificationType(notificationType);
             smsContext.setTransactionId(transactionId);
